2ef0a9ff2bcab7744cc53112455327b9b2bf7464
Yeshua Colon <yeshuax17@gmail.com>
2014-08-24 23:16:27 -0400

    Create Client.cpp

---
+++ src/Client/Client.cpp
@@ -0,0 +1,81 @@
+#include "Client.h"
+
+
+Client::Client()
+{
+
+
+}
+Client::~Client()
+{
+
+
+
+
+}
+int Client::run()
+{
+
+
+	WSAData wsa;
+	WORD DllVersion = MAKEWORD(2, 1);
+	if(initWinSock(wsa, DllVersion)) return START_ERROR;
+	SOCKET cConnect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);//create a soccet to connect to the server
+	SOCKADDR_IN sockaddr = {0};//create a client info struct for the server
+	sockaddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	sockaddr.sin_family = AF_INET;
+	sockaddr.sin_port = htons(123);
+	int len = sizeof(sockaddr);
+	
+	while (1)//look for the server
+	{
+
+		if(connect(cConnect, (SOCKADDR*)&sockaddr, len))//if found then break out of loop and go into send msg loop
+		{
+			cout << "You have connected to the server at 127.0.0.1(localhost)" << endl;
+			
+			
+			
+			break;
+			
+			
+		}
+		
+
+
+	}
+	sendMsgLoop(&cConnect);//send messages to loop until exit
+	
+	//shutdown client
+	shutdown(cConnect, 2);
+	closesocket(cConnect);
+	WSACleanup();
+	return 0;
+}
+
+
+
+int Client::sendMsgLoop(SOCKET *cConnect)
+{
+
+	while (1)
+	{
+
+		char buff[4096] = { 0 };//create a buffer for the message/messages we wan't to send
+
+		cout << "Send message: " << endl;//prompt the user for a message
+		cin.getline(buff, sizeof(buff));//get the line and store it in the buffer
+		if (strcmp(buff, "/exit") == 0)//if the buffer is equal to exit then break out of the loop and end the program
+		{
+
+			break;
+
+
+		}
+		
+		send(*cConnect, buff, strlen(buff) + 1, NULL);//send the message to the server
+
+	}
+	return 0;
+
+}
