bb3ac640ce4708759a996c146c392bb9d6b4cc9e
Yeshua Colon <yeshuax17@gmail.com>
2014-08-26 01:20:32 -0400

    Update stdserver.h

--- src/Server/Includes/stdserver.h
+++ src/Server/Includes/stdserver.h
@@ -1,7 +1,9 @@
 #pragma warning(disable:4996)
 #pragma comment(lib, "Ws2_32.lib")
-#include <Winsock.h>  
+#include <Winsock2.h>  
 #include <cstdlib>
 #include <string>
 #include <iostream>
+#include <process.h>
+//#define WIN32_LEAN_AND_MEAN
 using namespace std;
